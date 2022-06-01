a5b6595431d6c6619e021f7f6140d6c0a58485fd
Aleksey Romanov <a_romanov@wargaming.net>
2022-06-01 20:24:28 +0300

    add scripts for 2-phase tests

---
+++ run_pgbench_13_orioledb_2phase_short.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p pgbench_logs_orioledb
+shard_id=0
+export PGPASSWORD=orioledb_wgtest
+/usr/pgsql-13/bin/pgbench -h localhost -p5445 -U orioledb_wgtest --no-vacuum --progress=10 --jobs=5 \
+        --define=shard_num=$shard_id  --time=300 --client=50 \
+        --report-latencies -f pgbench_write_2phase_orioledb.sql  orioledb_wgtest > pgbench_logs_orioledb/pgbench_write_2phase_$shard_id_`date "+%Y-%m-%dT%H:%M:%S.%N"`.log 2>&1 
